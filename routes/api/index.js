8162d84f6bb6ba1207861eaf46d040911b803343
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-29 00:22:52 -0600

    add routes files and api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const friendRoutes = require('./friendRoutes');
+const reactionRoutes = require('./reactionRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/friends', friendRoutes);
+router.use('/reactions', reactionRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
