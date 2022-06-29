7067789cd2bc6387e4f9c72d97f01be70a578554
Kelly Walsh <kellywalsh827@gmail.com>
2022-06-29 00:46:17 -0600

    add friend route

---
+++ routes/api/friendRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {
+  addFriend,
+  removeFriend,
+} = require('../../controllers/friendController');
+
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend).delete(removeFriend);
+
+module.exports = router;
