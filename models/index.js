128c3768380ed945ffa23db5e90415ad629b9908
Kelly Walsh <kellywalsh827@gmail.com>
2022-07-03 22:50:54 -0600

    remove reactionSchema from models/index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
-const Reaction = require('./Reaction');
 const Thought = require('./Thought');
-const Friend = require('./Friend');
 
-module.exports = { User, Reaction, Thought, Friend };
+module.exports = {
+    User,
+    Thought
+};
